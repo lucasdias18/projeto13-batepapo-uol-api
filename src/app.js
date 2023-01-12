8334210adf2fc9b6fd16c058004f96a779bb78ac
Lucas Dias <lucasdias.ld10@gmail.com>
2023-01-12 17:34:17 -0300

    começando a implementar validação com a biblioteca joi

--- src/app.js
+++ src/app.js
@@ -1,20 +1,41 @@
 import express from "express"
 import cors from "cors"
-import { MongoClient } from "mongodb";
+import { MongoClient } from "mongodb"
+import dotenv from "dotenv"
+import joi from "joi"
+
+dotenv.config()
+
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-const participant = []
+const mongoClient = new MongoClient(process.env.DATABASE_URL)
+let db;
+
+mongoClient.connect().then(() => {
+	db = mongoClient.db()
+}).catch((err) => {
+    console.log('Deu ruim!')
+})
+
+// const participant = []
 
 app.post('/participants', (req, res) => {
 
     const nome = req.body.name
-    participant.push({name: nome})
+
+    const schema = Joi.object({
+        name: Joi.string().alphanum().min(3).required()
+    });
+
+    db.collection("participants").insertOne({ name: nome });
 
     res.send('Ok!')
 
 })
 
-app.listen(5000, () => console.log('subiu!!'))
+const PORT = 5000
+
+app.listen(PORT, () => console.log('subiu!!'))
